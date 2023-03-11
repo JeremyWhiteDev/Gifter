e1d3eab10c9649a516567d68c78ba55591596a2a
Jeremy White <jcwhite414@gmail.com>
2023-03-11 12:59:51 -0600

    Added UserProfileId prop

--- Gifter/Models/Comment.cs
+++ Gifter/Models/Comment.cs
@@ -4,6 +4,7 @@ public class Comment
 {
     public int Id { get; set; }
     public string Message { get; set; }
+    public int UserProfileId { get; set; }
     public  UserProfile UserProfile { get; set; }
     public int PostId { get; set; }
     public Post Post { get; set; }
